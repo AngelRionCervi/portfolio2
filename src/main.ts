12b3a327bb856564399fa1c212e25cc8c71004d1
AngelRionCervi <a.rioncervi@gmail.com>
2020-10-23 23:13:45 +0200

    clean-up

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { test } from "./test";
-import { html, render } from "lit-html";
-
-// Define a template
-const myTemplate = (name: any) => html`<p>Hello ${name}</p>`;
-
-// Render the template to the document
-render(myTemplate('World'), document.body);
